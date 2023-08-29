7300820ec612290ac47b7f5df5be03d5349a06be
ciscodeto <netofcisco@gmail.com>
2023-08-29 12:15:58 -0300

    Projeto MVVM

--- app/src/main/java/br/com/meu_primeiro_aplicativo/viewmodel/CepViewModel.kt
+++ app/src/main/java/br/com/meu_primeiro_aplicativo/viewmodel/CepViewModel.kt
@@ -1,7 +1,29 @@
 package br.com.meu_primeiro_aplicativo.viewmodel
 
 import androidx.lifecycle.ViewModel
+import br.com.meu_primeiro_aplicativo.model.CepResponse
+import br.com.meu_primeiro_aplicativo.repository.CepRepository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CepViewModel : ViewModel() {
+  private var repository : CepRepository = CepRepository()
 
+  fun getCep(cep : String,
+             onSuccess : (Response<CepResponse>) -> Unit,
+             onError: (Throwable) -> Unit) {
+
+    val call = repository.fetchCep(cep)
+    call.enqueue(object : Callback<CepResponse> {
+      override fun onResponse(call: Call<CepResponse>, response: Response<CepResponse>) {
+        onSuccess(response)
+      }
+
+      override fun onFailure(call: Call<CepResponse>, t: Throwable) {
+        onError(t)
+      }
+
+    })
+  }
 }
