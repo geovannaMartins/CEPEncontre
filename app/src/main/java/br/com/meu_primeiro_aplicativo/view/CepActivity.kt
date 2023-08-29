63ecf91078ac4005a51c2f33862bf87776550b62
Geovanna Martins <geovanna.jesus@f1rst.com.br>
2023-08-29 14:45:31 -0300

    feature: app consulta de cep

--- app/src/main/java/br/com/meu_primeiro_aplicativo/view/CepActivity.kt
+++ app/src/main/java/br/com/meu_primeiro_aplicativo/view/CepActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.AppCompatButton
@@ -84,8 +85,66 @@ class CepActivity : AppCompatActivity() {
   private fun getConfigCepAndCall() {
     val cep = etCep.text.toString().replace(".", "").replace("-", "")
     if (cep.isNotEmpty() && cep.length == 8) {
+      loading.visibility = View.VISIBLE
+      callCepService(cep)
       hideKeyboard()
+    } else {
+      Toast.makeText(
+        this,
+        "O CEP precisa conter 8 dígitos",
+        Toast.LENGTH_LONG
+      ).show()
     }
+  }
+
+  private fun callCepService(cep: String) {
+    viewModel.getCep(
+      cep = cep,
+      onSuccess = { cepResponse ->
+        loading.visibility = View.GONE
+        if (cepResponse.isSuccessful) {
+          setVisibilityResult(true)
+          val address = cepResponse.body()
+
+          if (address?.erro != true) {
+            tvDescriptionResult.text = String.format(
+              resources.getString(R.string.mpa_address),
+              address?.cep,
+              address?.logradouro,
+              address?.complemento,
+              address?.bairro,
+              address?.localidade,
+              address?.uf,
+              address?.ddd
+            )
+          } else {
+            setVisibilityResult(false)
+            Toast.makeText(
+              this,
+              resources.getString(R.string.mpa_error_cep),
+              Toast.LENGTH_LONG
+            ).show()
+          }
+          etCep.text.clear()
+        } else {
+          setVisibilityResult(false)
+          Toast.makeText(
+            this,
+            resources.getString(R.string.mpa_error_response),
+            Toast.LENGTH_SHORT
+          ).show()
+        }
+      },
+      onError = {
+        loading.visibility = View.GONE
+        setVisibilityResult(false)
+        Toast.makeText(
+          this,
+          resources.getString(R.string.mpa_error_comunication_api),
+          Toast.LENGTH_LONG
+        ).show()
+      }
+    )
   }
 
   private fun setVisibilityResult(responseSuccess: Boolean) {
--- app/src/main/java/br/com/meu_primeiro_aplicativo/viewmodel/CepViewModel.kt
+++ app/src/main/java/br/com/meu_primeiro_aplicativo/viewmodel/CepViewModel.kt
@@ -1,7 +1,34 @@
 package br.com.meu_primeiro_aplicativo.viewmodel
 
 import androidx.lifecycle.ViewModel
+import br.com.meu_primeiro_aplicativo.model.CepResponse
+import br.com.meu_primeiro_aplicativo.repository.CepRepository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CepViewModel : ViewModel() {
+  private var repository : CepRepository = CepRepository()
+
+  // Callback: é uma interface para receber a resposta de uma chamada a uma API REST
+  // Throwable: é um erro genérico para lidar com erros não mapeados
+
+  fun getCep(
+    cep: String,
+    onSuccess: (Response<CepResponse>) -> Unit,
+    onError: (Throwable) -> Unit
+  ) {
+    val call = repository.fetchCep(cep)
+
+    call.enqueue(object : Callback<CepResponse> {
+      override fun onResponse(call: Call<CepResponse>, response: Response<CepResponse>) {
+        onSuccess(response)
+      }
+
+      override fun onFailure(call: Call<CepResponse>, t: Throwable) {
+        onError(t)
+      }
+    })
+  }
 
 }
