b1e14cb78b19db878f4c88445303a657995d7b09
lauraluch <lauraluchiari@hotmail.com>
2023-08-29 11:55:57 -0300

    feat: fazendo requisição com API

--- app/src/main/java/br/com/meu_primeiro_aplicativo/repository/CepRepository.kt
+++ app/src/main/java/br/com/meu_primeiro_aplicativo/repository/CepRepository.kt
@@ -1,5 +1,22 @@
 package br.com.meu_primeiro_aplicativo.repository
 
+import br.com.meu_primeiro_aplicativo.model.CepResponse
+import br.com.meu_primeiro_aplicativo.network.CepService
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class CepRepository {
 
+  // Retrofit: é uma biblioteca Android que simplifica o consumo de API Rest.
+
+  private var retrofit : Retrofit = Retrofit.Builder()
+    .baseUrl("https://viacep.com.br/")
+    .addConverterFactory(GsonConverterFactory.create()).build()
+
+  fun fetchCep(cep: String) : Call<CepResponse> {
+    val cepService = retrofit.create(CepService::class.java)
+
+    return cepService.getCep(cep)
+  }
 }
